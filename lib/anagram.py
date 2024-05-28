3553df9c63bc7aaaca4db9dc314bba55d97e66b9
+ <omuhindaaaron@gmail.com>
2024-05-28 21:53:40 +0300

    feat: Enter new data

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagrams =[]
+        for word in word_list:
+            if sorted(self.word) == sorted(word):
+                anagrams.append(word)
+        return anagrams
